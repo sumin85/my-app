a10b30d3e9485e353a878c306b718ba3af0c26e0
SUMIN <lsm041224@gmail.com>
2025-07-01 15:07:40 +0900

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  apps:[{
-    name: [{
-      name: 'my-app',
-      script: './app.js',
+  apps: [
+    {
+      name: "my-app",
+      script: "app.js",
       instances: 1,
       autorestart: true,
-      watch: false
-  }]
+      watch: false,
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
 };
