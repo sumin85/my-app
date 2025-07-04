610cba57a47a9ee97d7d51debc3d242d2a5d5e09
SUMIN <lsm041224@gmail.com>
2025-07-04 16:22:13 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'd우ㄴㅇㄹㄷㄹㄴㅇ' });
+  res.render('index', { title: 'ㅈㅣㅂ갈래요' });
 });
 
 module.exports = router;
