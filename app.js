b3fe3880245a28e606607e5392176e2878ca219f
sumin85 <lsm041224@gmail.com>
2025-07-08 13:25:44 +0900

    routes 연결 수정

--- app.js
+++ app.js
@@ -42,4 +42,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+console.log("실행중");
+
 module.exports = app;
