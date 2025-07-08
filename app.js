fbc4b9293d138d80149c3c8bd9f52868f4065df0
sumin85 <lsm041224@gmail.com>
2025-07-08 12:56:38 +0900

    수정

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+
 
 var app = express();
 const cors = require('cors');
--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res)  => {
-  const users = [
+  res.json([
     { id: 1, name: 'a'},
     { id: 2, name: 'b'},
     { id: 3, name: 'c'}
-  ];
-  res.json(users);
+  ]);
 });
 
 module.exports = router;
