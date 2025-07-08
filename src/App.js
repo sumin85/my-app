3103b47a12ec7f361144040cdfc412b412a0c519
sumin85 <lsm041224@gmail.com>
2025-07-08 10:44:47 +0900

    react 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://52.79.186.21:3000/api/users")
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  },[]);
+  
+  return(
+    <div>
+      <h2>유저목록</h2>
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
     </div>
   );
 }
