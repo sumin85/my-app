648d422b6f9cb95200ea1a4ba4af3ebe7e1cd0f6
sumin85 <lsm041224@gmail.com>
2025-07-08 15:56:12 +0900

    build 생성

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/Home/HomePage';
 
 function App() {
   return(
     <Router>
       <Routes>
-         <Route path='/' element={<HomePage />} />
       </Routes>
     </Router>
   );
