f3471043ea79edf36f36d510f852f1b844a57737
sumin85 <lsm041224@gmail.com>
2025-07-08 10:58:43 +0900

    app.js 수정

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const cors = require('cors');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
@@ -37,13 +39,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`server running`);
-});
-
-const cors = require('cors');
-app.use(cors());
 
 module.exports = app;
